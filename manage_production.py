0382f6ef42c72e3b18369916c1e85988ee1c3006
BradHagstrom16 <bhagstrom0@gmail.com>
2025-09-02 17:06:51 -0500

    Add files via upload

--- manage_production.py
+++ manage_production.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 class ProductionManager:
     def __init__(self):
-        self.username = "B1GBrad"  # PythonAnywhere username
+        self.username = "YOURUSERNAME"  # PythonAnywhere username
         self.local_db = "picks.db"
         self.backup_dir = "production_backups"
         
